b72f14f669fcd9324a8e8d0fe4f8fc9a2e6072e3
Fabiplooh <fabvbbcours@gmail.com>
2025-04-22 11:32:25 +0200

    Ajout de la case selectionné en pointillé animé

--- src/Vue/GaufreVue.java
+++ src/Vue/GaufreVue.java
@@ -13,9 +13,24 @@ public class GaufreVue extends JComponent implements Observateur {
     public static final Color GREEN = new Color(62, 104, 8);
     public static final Color GRAY = new Color(119, 119, 119);
     public static final Color YELLOW= new Color(140, 45, 45);
+    private Timer timer;
+    private int curLinePix;
+    private int curColPix;
+    private float decalagePointille =0.0f;
 
     public GaufreVue(GaufreModele modele) {
         this.modele = modele;
+        timer = new Timer(50, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                decalagePointille +=1.0f;
+                if (decalagePointille >10.0f){
+                    decalagePointille = 0.0f;
+                }
+                repaint();
+            }
+        });
+        timer.start();
+
     }
 
     public void setAdaptateurSouris(AdaptateurSouris souris) {
@@ -37,6 +52,7 @@ public class GaufreVue extends JComponent implements Observateur {
     }
 
     private void DessineGaufre(Graphics g) {
+        boolean flag =false;
         int largeur = getWidth()/ modele.getColonne();
         int hauteur = getHeight()/ modele.getLine();
 
@@ -59,12 +75,42 @@ public class GaufreVue extends JComponent implements Observateur {
                         remplisCase(g, x, y, largeur, hauteur, YELLOW);
                     }
                 }
+
                 g.setColor(Color.black);
-                g.drawRect(x,y,largeur,hauteur);
+                g.drawRect(x, y, largeur, hauteur);
+
+                if (modele.isSelectedCase(i, j)) {
+                    flag = true;
+                    curLinePix = x;
+                    curColPix = y;
+                }
             }
         }
+        if (flag){ //si le joueur a la souris au dessus d'une case
+            selectedCase(g, curLinePix, curColPix, largeur, hauteur);
+        }
     }
-  
+
+    private void selectedCase(Graphics g, int x, int y, int largeur, int hauteur) {
+        Graphics2D g2d = (Graphics2D) g;
+        //Tableau avec taille trait, taille espace
+        float[] traitsEspace = {5.0f, 5.0f};
+        //Permet de definir une nouvelle brosse pour dessiner.
+        BasicStroke brossePointillé = new BasicStroke(
+                1.0f,                      // Épaisseur de ligne
+                BasicStroke.CAP_BUTT,      // Style des extrémités
+                BasicStroke.JOIN_MITER,    // Style des jonctions
+                10.0f,                     // Limite de mitre
+                traitsEspace,                      // Motif de pointillé
+                decalagePointille                      // Phase de début
+        );
+        g2d.setStroke(brossePointillé);
+        g2d.setColor(Color.MAGENTA);
+        g2d.drawRect(x,y,largeur,hauteur);
+        BasicStroke brosseClassique = new BasicStroke();
+        g2d.setStroke(brosseClassique);
+    }
+
     private void remplisCase(Graphics g, int x, int y, int largeur, int hauteur, Color couleur){
         g.setColor( couleur );
         g.fillRect(x,y,largeur,hauteur);
